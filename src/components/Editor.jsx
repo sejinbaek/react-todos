e8e83093a3af77e577b9822bf0c8e7efb48e0f94
sejinbaek <sejin9767@gmail.com>
2025-05-04 13:49:36 +0900

    ✨ feat: 투두리스트 추가 기능 구현하기

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState();
+
+  // content에 입력값 저장하기
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+  // 할 일 추가하기
+  const onSubmit = () => {
+    onCreate(content);
+  };
+
   return (
     <div className="Editor">
-      <input type="text" placeholder="새로운 할 일을 입력하세요" />
-      <button>추가</button>
+      <input
+        type="text"
+        placeholder="새로운 할 일을 입력하세요"
+        value={content}
+        onChange={onChangeContent}
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
