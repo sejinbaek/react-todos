8d734e7f59af9b245dfa6e33c0a8bfb0679a8dee
sejinbaek <sejin9767@gmail.com>
2025-05-04 22:40:53 +0900

    📦 chore: 불필요한 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,10 @@ import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
-const mockData = [
-  {
-    id: 0, // 아이템을 구별하기 위한 ID
-    isDone: false,
-    content: "리액트 공부하기",
-    date: new Date().getTime(), // 날짜는 타임스탬프로 저장하기
-  },
-  {
-    id: 1,
-    isDone: false,
-    content: "투두리스트 만들기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 2, // 아이템을 구별하기 위한 ID
-    isDone: false,
-    content: "놀러 가기",
-    date: new Date().getTime(),
-  },
-];
-
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, setTodos] = useState([]);
 
-  const idRef = useRef(3);
+  const idRef = useRef(1);
 
   // 새로운 투두 아이템 생성하기 (Create)
   const onCreate = (content) => {
