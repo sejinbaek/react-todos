7ed1cb1d12aaef54923ee911f89610a91cb8e122
sejinbaek <sejin9767@gmail.com>
2025-05-04 14:42:15 +0900

    ✨ feat: 투두리스트 조회 기능 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
   return (
     <div className="List">
       <h4>Todo List ⭐</h4>
       <input placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem todo={todo} key={todo.id} />
+        ))}
       </div>
     </div>
   );
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./TodoItem.css";
 
-const TodoItem = () => {
+const TodoItem = ({ todo }) => {
+  const date = new Date(todo.date).toLocaleDateString();
   return (
     <div className="TodoItem">
       <input type="checkbox" />
-      <div className="content">할 일</div>
-      <div className="date">날짜</div>
+      <div className="content">{todo.content}</div>
+      <div className="date">{date}</div>
       <button>삭제</button>
     </div>
   );
