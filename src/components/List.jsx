471f57b271e20b1e66c923908a0a85fb8daa7c29
sejinbaek <sejin9767@gmail.com>
2025-05-04 14:59:21 +0900

    ✨ feat: 투두리스트 검색 기능 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
 const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilterdDate = () => {
+    if (!search) {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      // 찾는 값과 검색 값 모두 소문자로 바꾸고 필터링하기
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilterdDate();
+
   return (
     <div className="List">
       <h4>Todo List ⭐</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <TodoItem todo={todo} key={todo.id} />
         ))}
       </div>
